1e8b13c8ddf06d929ac4326cba8f51289b49aa53
ravi812-0510 <ravirajvala812@gmail.com>
2026-07-27 22:47:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 // Sweetuu ❤️ Rasmalaiiii
-console.log("Our Love Story Begins ❤️");
+
+const enterBtn = document.getElementById("enterBtn");
+
+if (enterBtn) {
+    enterBtn.addEventListener("click", () => {
+        alert(
+`❤️ Welcome Rasmalaiiii ❤️
+
+Our love story is just beginning...
+
+From your Sweetuu 💕`
+        );
+
+        document.querySelector(".timeline").scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+}
